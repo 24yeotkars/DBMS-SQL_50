58503d157baaf6fe78ca7c294547d329e1268c0d
24yeotkars <24yeotkars@rbunagpur.in>
2026-03-20 18:14:39 +0530

    Leetcode 1731. The Number of Employees Which...

---
+++ sql41.sql
@@ -0,0 +1,11 @@
+-- Leetcode 1731. The Number of Employees Which Report to Each Employee
+SELECT 
+    e.employee_id,
+    e.name,
+    COUNT(r.employee_id) AS reports_count,
+    ROUND(AVG(r.age)) AS average_age
+FROM Employees e
+JOIN Employees r
+    ON e.employee_id = r.reports_to
+GROUP BY e.employee_id, e.name
+ORDER BY e.employee_id;
